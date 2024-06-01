0b4fdc5602e241d994090934e18f3fc94aebc50a
TanyaVerb <karra_t@mail.ru>
2024-06-02 00:57:55 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const sliderWrapper = document.querySelector(".slider__container");
-const sliderLine = document.querySelector(".slider__iine");
+const sliderLine = document.querySelector(".slider__line");
 const images = document.querySelectorAll("img");
 const dots = document.querySelectorAll(".slider__dot");
+console.log(dots[0]);
 const nextBtn = document.querySelector(".slider__next-btn");
 const prevBtn = document.querySelector(".slider__prev-btn");
 
@@ -9,6 +10,8 @@ const prevBtn = document.querySelector(".slider__prev-btn");
 let orderImg = 0;
 let width;
 console.log(width);
+
+// dots[orderImg].classList.add("active");
 
 function setWidthLine() {
   width = sliderWrapper.offsetWidth;
@@ -22,22 +25,47 @@ function setWidthLine() {
   console.log("widthLine", sliderLine.style.width);
 }
 
+//срабатывает при изменении размера представления документа (окна)
 window.addEventListener("resize", setWidthLine);
 setWidthLine();
 
+//___
+function dotActive() {
+  dots[orderImg].classList.add("active");
+}
+
+function removeActiveClass() {
+  dots[orderImg].classList.remove("active");
+}
+
+//__
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    const value = index * width;
+    sliderLine.style.transform = `translate(-${value}px)`;
+  });
+});
+
+//___
+
 nextBtn.addEventListener("click", function () {
+  removeActiveClass();
   orderImg++;
   if (orderImg > images.length - 1) {
     orderImg = 0;
   }
+  dotActive();
+
   scrollSlider();
 });
 
 prevBtn.addEventListener("click", function () {
+  removeActiveClass();
   orderImg--;
   if (orderImg < 0) {
     orderImg = images.length - 1;
   }
+  dotActive();
 
   scrollSlider();
 });
